0ee78abeaa30d456f020fcc184ccfa8df07ef880
chris <chris@debian-mac.local>
2018-06-03 21:38:45 -0700

    home

--- src/main/java/com/swgas/nexus/Main.java
+++ src/main/java/com/swgas/nexus/Main.java
@@ -10,20 +10,22 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Main {
-    private static final Logger LOG = Logger.getLogger(Main.class.getName());
-    public static final String BASE_URI = "http://0.0.0.0:8080/nexus/";
-    
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        HttpHandler httpHandler = new CLStaticHttpHandler(HttpServer.class.getClassLoader(), "/");
-        server.getServerConfiguration().addHttpHandler(httpHandler, "/");
-        LOG.info(String.format("%nNexus microservices started.%nWADL available at %sapplication.wadl%nHit enter to stop it...%n", BASE_URI));
-        System.in.read();
-        server.shutdownNow();
-    }
-    
-    public static HttpServer startServer() {
-        final ResourceConfig rc = new ResourceConfig().packages("com.swgas.nexus.rest");
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+	private static final Logger LOG = Logger.getLogger(Main.class.getName());
+	public static final String BASE_URI = "http://0.0.0.0:8080/nexus/";
+
+	public static void main(String... args) throws IOException {
+		final HttpServer server = startServer();
+		HttpHandler httpHandler = new CLStaticHttpHandler(HttpServer.class.getClassLoader(), "/");
+		server.getServerConfiguration().addHttpHandler(httpHandler, "/");
+		LOG.info(String.format(
+				"%nNexus microservices started.%nWADL available at %sapplication.wadl%nHit enter to stop it...%n",
+				BASE_URI));
+		System.in.read();
+		server.shutdownNow();
+	}
+
+	public static HttpServer startServer() {
+		final ResourceConfig rc = new ResourceConfig().packages("com.swgas.nexus.rest");
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+	}
 }
--- src/main/java/com/swgas/nexus/rest/Login.java
+++ src/main/java/com/swgas/nexus/rest/Login.java
@@ -27,29 +27,28 @@ import javax.ws.rs.core.Response;
  */
 @Path("login")
 public class Login {
-  private static final String CLASS = Login.class.getName();
-  private static final Logger LOG   = Logger.getLogger(CLASS);
+	private static final String CLASS = Login.class.getName();
+	private static final Logger LOG = Logger.getLogger(CLASS);
 
-  @POST
-  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-  public Response login(@FormParam("email") String email, @FormParam("password") String password) {
-    LOG.info(String.format("email: %s; password: %s", email, password));
-    //lookup user and set cookie with webCustomerId
-    return Response.seeOther(URI.create("../home.html")).cookie(new NewCookie("webCustomerUid", "1601", "/", null, 2, "", 1000, Date.from(Instant.now().plus(1, ChronoUnit.DAYS)), false, false)).build();
-  }
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response login(@FormParam("email") String email, @FormParam("password") String password) {
+		LOG.info(String.format("email: %s; password: %s", email, password));
+		// lookup user and set cookie with webCustomerId
+		return Response.seeOther(URI.create("../home.html")).cookie(new NewCookie("webCustomerUid", "1601", "/", null,
+				2, "", 1000, Date.from(Instant.now().plus(1, ChronoUnit.DAYS)), false, false)).build();
+	}
 
-  @Path("{id}")
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response loginCustomer(@PathParam("id") long customerUid) {    
-    return Response.ok(
-      Json.createObjectBuilder()
-      .add("customerName", "John DeFwan")
-      .add("address", "7301 Super Fake St. Las Nevgas, NV.")
-      .add("accountNumber", "211-7972978-002")
-      .add("totalPaymentAmount", 14.78)
-      .add("paymentDueDate", LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("MMMM, dd yyyy")))
-      .build().toString()
-    ).build();
-  }
+	@Path("{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response loginCustomer(@PathParam("id") long customerUid) {
+		return Response.ok(Json.createObjectBuilder()
+				.add("customerName"      , "John DeFwan")
+				.add("address"           , "7301 Super Fake St. Las Nevgas, NV.")
+				.add("accountNumber"     , "211-7972978-002")
+				.add("totalPaymentAmount", 14.78)
+				.add("paymentDueDate"    , LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("MMMM, dd yyyy")))
+				.build().toString()).build();
+	}
 }
